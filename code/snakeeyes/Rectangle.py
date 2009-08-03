1cfe8d653c2a2cd56f141b0701161f350226815a
Michal Wallace <michal.wallace@gmail.com>
2009-08-03 03:26:36 +0000

    Minor source cleanups to Rectangle.
    git-svn-id: file:///svn/svn/snakeeyes/trunk@13 6d29e5fb-0a70-0410-b964-dfb56dcffc1b

--- code/snakeeyes/Rectangle.py
+++ code/snakeeyes/Rectangle.py
@@ -1,16 +1,19 @@
 
 class Rectangle(object):
     """
+    A simple data class.
     """
     def __init__(self, pos, size):
         self.pos = pos
         self.size = size
     
     def far_corner(self):
-        left, top, right, bottom = self.as_quad()
+        ":: self -> (right, bottom)"
+        _, _, right, bottom = self.as_quad()
         return (right, bottom)
     
     def as_quad(self):
+        ":: self -> (left, top, right, bottom)"
         left, top = self.pos
         width, height = self.size
         return (left, top, left + width, top + height)
