53587d52724d81fa88108f13f4e56e925f8f56c5
Michal Wallace <michal.wallace@gmail.com>
2009-08-03 04:09:29 +0000

    moved capture.py to gui package and did a little cleaning.
    git-svn-id: file:///svn/svn/snakeeyes/trunk@18 6d29e5fb-0a70-0410-b964-dfb56dcffc1b

--- code/snakeeyes/gui/capture.py
+++ code/snakeeyes/gui/capture.py
@@ -1,3 +1,13 @@
+"""
+This app lets you collect snapshots of a window for
+processing later.
+
+Enter an hwnd in the text box and click the button to
+take a picture.
+
+@TODO: tie this in with WindowSelector!
+@TODO: move output directory config into the GUI
+"""
 
 from wx.py.shell import ShellFrame
 import scrape, windows
@@ -5,21 +15,24 @@ import math, os, time, math, wx
 import win32gui
 
 IMGROOT = "/tmp"
-WAITING, CAPTURED = 0,1
+WAITING, CAPTURED = 0, 1
 
 
 class CaptureFrame(wx.Frame):
+    """
+    Click the button to take a picture. :)
+    """
     def __init__(self, hwnd, *a, **kw):
+        
         wx.Frame.__init__(self, *a, **kw)
 
         box = wx.BoxSizer(wx.VERTICAL)
         box.Add(wx.StaticText(self, -1, 'enter the hwnd and click capture'))
-
         self.txt = wx.TextCtrl(self, -1, str(hwnd))
         box.Add(self.txt, 0, wx.EXPAND)
 
         b = wx.Button(self, wx.NewId(), "capture")
-        self.Bind(wx.EVT_BUTTON, self.grabImage, b)
+        self.Bind(wx.EVT_BUTTON, self.capture, b)
         box.Add(b, 5, wx.EXPAND)
 
         self.SetSizer(box)
@@ -40,8 +53,9 @@ class CaptureFrame(wx.Frame):
         self.state = WAITING
         self.Bind(wx.EVT_IDLE, self.OnIdle)
 
-    def imageDir(self):
-        imdir = "%shwnd%s" % (IMGROOT, self.targetHwnd())
+
+    def image_dir(self):
+        imdir = "%shwnd%s" % (IMGROOT, self.hwnd)
         if not os.path.exists(imdir):
             os.mkdir(imdir)
         return imdir
@@ -50,26 +64,28 @@ class CaptureFrame(wx.Frame):
         # @TODO: override this to schedule the captures
         return False
 
+
     def OnIdle(self, e):
-        if not self.txt.Value: return
+        if not self.hwnd: return
         # and give the window a chance to redraw:
-        
         time_to_capture = self.is_time_to_capture()
         if time_to_capture and self.state == WAITING:
             self.state = CAPTURED
-            self.grabImage()
+            self.capture()
 
         elif self.state==CAPTURED and not time_to_capture:
             self.state = WAITING
 
-    def targetHwnd(self):
+    @property
+    def hwnd(self):
         return int(self.txt.Value)
 
-    def nextName(self):
+
+    def next_name(self):
 
         if self.lastCount is None:
             files = sorted([
-                f for f in os.listdir(self.imageDir())
+                f for f in os.listdir(self.image_dir())
                 if f.endswith('.png')])
             if files:
                 self.lastCount = int(files[-1].split('.')[0])
@@ -77,11 +93,12 @@ class CaptureFrame(wx.Frame):
                 self.lastCount = -1
 
         self.lastCount += 1
-        return '%s/%05i.png' % (self.imageDir(), self.lastCount)
+        return '%s/%05i.png' % (self.image_dir(), self.lastCount)
 
-    def grabImage(self, e=None):
-        self.im = windows.Window(self.targetHwnd()).as_image()
-        imName = self.nextName()
+
+    def capture(self, e=None):
+        self.im = windows.Window(self.hwnd).as_image()
+        imName = self.next_name()
         self.im.save(imName)
         print "saved %s" % imName
         if e==None:
